536e07f6752c80fa39011d133fb764e48768be3b
POO <taty-1997-02@outlook.com>
2024-05-23 09:52:22 -0500

    CLSE PERSONAS

--- CLASE_HERENCIA.java
+++ CLASE_HERENCIA.java
@@ -7,7 +7,7 @@ public class CLASE_HERENCIA {
 
     public CLASE_HERENCIA(String nombre, String apellido,int telefono) {
         this.nombre = nombre;
-        this.apellido = nombre;
+        this.apellido = apellido;
         this.telefono = telefono;
     }
     public void imprimir_info(){
@@ -26,9 +26,25 @@ class Herencia1 extends CLASE_HERENCIA{
         this.hobbie=hobbie;
         this.comi_favo=comi_favo;
     }
-    public void imprimir_completo(){
+    public void imprimir_completo() {
         super.imprimir_info();
-        System.out.println("Hobbie "+hobbie);
-        System.out.println("Comida Favorita "+comi_favo);
+        System.out.println("Hobbie " + hobbie);
+        System.out.println("Comida Favorita " + comi_favo);
+    }
+}
+class Herencia2 extends Herencia1{
+    public String tiempo_en_redes;
+    public String redes_favo;
+
+    public Herencia2 (String nombre, String apellido, int telefono,String hobbie, String comi_favo, String tiempo_en_redes,String redes_favo){
+        super(nombre,apellido,telefono,hobbie,comi_favo);
+        this.tiempo_en_redes=tiempo_en_redes;
+        this.redes_favo=redes_favo;
+    }
+    public void imprimir_red_social() {
+        System.out.println("Información con Red Social ");
+        super.imprimir_info();
+        System.out.println("Tiempo en redes  " + tiempo_en_redes);
+        System.out.println("Red social Favorita " + redes_favo);
     }
 }
--- Class_print.java
+++ Class_print.java
@@ -2,7 +2,12 @@ package Ejercicioclase23_05;
 
 public class Class_print {
     public static void main (String[] args){
+        //Heredado del Padre
         Herencia1 person1 = new Herencia1("Eve","Guachamin",3393346,"Ver Series","Seco de pollo");
         person1.imprimir_completo();
+        System.out.println("----------------------------------");
+        //Herencia de la hija
+        Herencia2 persona2 = new Herencia2("Taty","Guachamin",992801667,"Jugar Basket","Lasaña","4 horas por dia","Tik Tok");
+        persona2.imprimir_red_social();
     }
 }

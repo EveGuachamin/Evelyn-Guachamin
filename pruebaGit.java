58e7a6b924e72f24216956e957a5f59fc5ba4021
POO <taty-1997-02@outlook.com>
2024-05-23 08:35:19 -0500

    Semana5-cambio

--- pruebaGit.java
+++ pruebaGit.java
@@ -3,5 +3,7 @@ package Ejercicioclase23_05;
 public class pruebaGit {
     public static void main (String[] args){
         System.out.println("Hola mundo");
+        System.out.println("Hola, soy programador");
     }
+
 }
